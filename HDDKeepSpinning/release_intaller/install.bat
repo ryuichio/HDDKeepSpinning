4cfcf61d03b751f123a6c02a457d076bcf7b4e84
Ryuichi Oana <oana.ryuichi@gmail.com>
2019-01-03 19:21:01 +0900

    fixed faild installation with running bat from explorer not in cmd prompt

--- HDDKeepSpinning/release_intaller/install.bat
+++ HDDKeepSpinning/release_intaller/install.bat
@@ -1,6 +1,7 @@
 @echo off
 openfiles > NUL 2>&1
 if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
+cd /d %~dp0
 %windir%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe HDDKeepSpinning.exe
 sc config HDDKeepSpinning start=delayed-auto
 net start HDDKeepSpinning
@@ -8,3 +9,4 @@ goto End
 :NotAdmin 
 echo [ERROR] Needed to run as Administrator
 :End
+pause
--- HDDKeepSpinning/release_intaller/uninstall.bat
+++ HDDKeepSpinning/release_intaller/uninstall.bat
@@ -1,9 +1,11 @@
 @echo off
 openfiles > NUL 2>&1
 if NOT %ERRORLEVEL% EQU 0 goto NotAdmin
+cd /d %~dp0
 net stop HDDKeepSpinning
 %windir%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /u HDDKeepSpinning.exe
 goto End
 :NotAdmin 
 echo [ERROR] Needed to run as Administrator
 :End
+pause
